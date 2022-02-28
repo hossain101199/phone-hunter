2bbbed05ddae9023be0a6f9ad5f3c8af6cbcf434
hossain101199 <hossainbhai1011@gmail.com>
2022-03-01 04:01:49 +0600

    api fatch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+const input =document.getElementById("inpute-value").value;
+const resultContainer =document.getElementById("result-container")
+
 const searchbutton=()=>{
-console.log("buton test")
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${input}`)
+    .then(res => res.json())
+    .then(data => phoneResult(data.data))
 }
+
+const phoneResult =(phones)=>{
+for(const phone of phones){
+    console.log(phone.brand)
+    console.log(phone.phone_name)
+const div = document.createElement("div");
+div.classList.add("col-lg-4");
+div.classList.add("mb-2");
+div.innerHTML=`
+<div class="card border-primary m-3" style="width: 18rem;">
+        <img src="${phone.image}" style="width: 14rem;" class="card-img-top img-fluid m-3">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <h6 class="card-text">Brand: ${phone.brand}</h6>
+          <a class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+`
+resultContainer.appendChild(div);
+}
+}
+
