d5c190025da8660573f2c415d49a5d69aab305ad
hossain101199 <hossainbhai1011@gmail.com>
2022-03-01 06:15:47 +0600

    api fetch1

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
-const input =document.getElementById("inpute-value").value;
-const resultContainer =document.getElementById("result-container")
+
+let error =document.getElementById("errormsg");
+const resultContainer =document.getElementById("result-container");
 
 const searchbutton=()=>{
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${input}`)
-    .then(res => res.json())
-    .then(data => phoneResult(data.data))
+    let input =document.getElementById("inpute-value").value;
+    if(input==""){
+        error.innerText="You have to search by name";
+
+        input="";
+        resultContainer.innerHTML="";
+    }
+    else{
+        resultContainer.innerHTML="";
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${input}`)
+        .then(res => res.json())
+        .then(data => phoneResult(data.data))
+
+        input="";
+        error.innerText="";
+        
+    }
 }
 
 const phoneResult =(phones)=>{
-for(const phone of phones){
-    console.log(phone.brand)
-    console.log(phone.phone_name)
+const the20Result = phones.slice(0, 20);
+for(const phone of the20Result){
 const div = document.createElement("div");
 div.classList.add("col-lg-4");
 div.classList.add("mb-2");
@@ -20,7 +34,7 @@ div.innerHTML=`
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <h6 class="card-text">Brand: ${phone.brand}</h6>
-          <a class="btn btn-primary">Go somewhere</a>
+          <a class="btn btn-primary">See Details</a>
         </div>
       </div>
 `
@@ -28,3 +42,11 @@ resultContainer.appendChild(div);
 }
 }
 
+
+// fetch(`https://openapi.programming-hero.com/api/phones?search=`)
+//         .then(res => res.json())
+//         .then(data => console.log(data.data))
+
+// fetch(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_mini-11104`)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
