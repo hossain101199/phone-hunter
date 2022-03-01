89972037db78235265a13c3e36b1300cf0dabad1
hossain101199 <hossainbhai1011@gmail.com>
2022-03-01 20:55:59 +0600

    last edition

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 
 let error =document.getElementById("errormsg");
 const resultContainer =document.getElementById("result-container");
+
+let spinar = document.getElementById("spinnerid")
+spinar.style.display= "block"
+spinar.style.display= "none"
+
 // search btn .................................................................................................................
 const searchbutton=()=>{
   resultContainer.innerHTML="";
@@ -60,17 +65,16 @@ const seeDetailsbox =(allinfo)=>{
             <div class="card-body">
               <h5 class="card-title">${allinfo.name}</h5>
               <h6 class="card-text"><b>Brand:</b> ${allinfo.brand}</h6>
-              <p class="card-text"><b>Release Date:</b> ${allinfo.releaseDate}</p>
-              <p class="card-text"><b>Sensors:</b> ${allinfo.mainFeatures.sensors}</p>
-              <p class="card-text"><b>Chip Set:</b> ${allinfo.mainFeatures.chipSet}</p>
+              <p class="card-text"><b>Release Date:</b> ${allinfo.releaseDate? allinfo.releaseDate: "Sorry, we didn't find the release date"}</p>
+              <p class="card-text"><b>Sensors:</b> ${allinfo.mainFeatures.sensors? allinfo.mainFeatures.sensors: "Sorry, we didn't find the sensors"}</p>
+              <p class="card-text"><b>Chip Set:</b> ${allinfo.mainFeatures.chipSet? allinfo.mainFeatures.chipSet: "Sorry, we didn't find the chip Set"}</p>
               <p class="card-text"><b>Display Size:</b> ${allinfo.mainFeatures.displaySize}</p>
               <p class="card-text"><b>Memory:</b> ${allinfo.mainFeatures.memory}</p>
               <p class="card-text"><b>Storage:</b> ${allinfo.mainFeatures.storage}</p>
-              <p class="card-text"><b>Others:</b> <u>Bluetooth:</u> ${allinfo?.others?.Bluetooth}, <u>GPS:</u> ${allinfo?.others?.GPS}, <u>NFC:</u> ${allinfo?.others?.NFC}, <u>Radio:</u> ${allinfo?.others?.Radio}, <u>USB:</u> ${allinfo?.others?.USB}, <u>WLAN:</u> ${allinfo?.others?.WLAN},</p>
+              <p class="card-text"><b>Others:</b> <u>Bluetooth:</u> ${allinfo?.others?.Bluetooth? allinfo.others.Bluetooth:"unavailable"}, <u>GPS:</u> ${allinfo?.others?.GPS? allinfo.others.GPS: "unavailable"}, <u>NFC:</u> ${allinfo?.others?.NFC? allinfo.others.NFC: "unavailable"}, <u>Radio:</u> ${allinfo?.others?.Radio? allinfo.others.Radio: "unavailable"}, <u>USB:</u> ${allinfo?.others?.USB? allinfo.others.USB: "unavailable"}, <u>WLAN:</u> ${allinfo?.others?.WLAN? allinfo.others.WLAN: "unavailable"},</p>
             </div>
           </div>
     `
     error.appendChild(div);
 }
 // END-------------------------------------------------------------------------------------------------
-
