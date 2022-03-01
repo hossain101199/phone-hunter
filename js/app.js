1a3285d9adbc302e85dda4c5b06cfc3df3d7ca5e
hossain101199 <hossainbhai1011@gmail.com>
2022-03-01 08:39:11 +0600

    main part is ok

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
 
 let error =document.getElementById("errormsg");
 const resultContainer =document.getElementById("result-container");
-
+// search .................................................................................................................
 const searchbutton=()=>{
+  resultContainer.innerHTML="";
     let input =document.getElementById("inpute-value").value;
     if(input==""){
-        error.innerText="You have to search by name";
-
-        input="";
-        resultContainer.innerHTML="";
+        error.innerHTML=`<h4>You have to search by name</h4>`;
     }
     else{
-        resultContainer.innerHTML="";
         fetch(`https://openapi.programming-hero.com/api/phones?search=${input}`)
         .then(res => res.json())
         .then(data => phoneResult(data.data))
-
         input="";
-        error.innerText="";
-        
+        error.innerHTML="";
     }
 }
 
 const phoneResult =(phones)=>{
+
 const the20Result = phones.slice(0, 20);
 for(const phone of the20Result){
 const div = document.createElement("div");
@@ -34,19 +30,40 @@ div.innerHTML=`
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <h6 class="card-text">Brand: ${phone.brand}</h6>
-          <a class="btn btn-primary">See Details</a>
+          <a href="#" onclick="seeDetailsbutton('${phone.slug}')" class="btn btn-primary">See Details</a>
         </div>
       </div>
 `
 resultContainer.appendChild(div);
 }
 }
+// see details ..............................................................................
+const seeDetailsbutton = (slug)=>{
+  error.innerHTML="";
+fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        .then(res => res.json())
+        .then(data => seeDetailsbox(data.data))
+}
 
-
-// fetch(`https://openapi.programming-hero.com/api/phones?search=`)
-//         .then(res => res.json())
-//         .then(data => console.log(data.data))
-
-// fetch(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_mini-11104`)
-//         .then(res => res.json())
-//         .then(data => console.log(data))
+const seeDetailsbox =(allinfo)=>{
+    const div = document.createElement("div");
+    div.classList.add("col-12");
+    div.classList.add("mb-2");
+    div.innerHTML=`
+    <div class="card border-info m-3">
+            <img src="${allinfo.image}" style="width: 14rem;" class="card-img-top img-fluid m-3">
+            <div class="card-body">
+              <h5 class="card-title">${allinfo.name}</h5>
+              <h6 class="card-text"><b>Brand:</b> ${allinfo.brand}</h6>
+              <p class="card-text"><b>Release Date:</b> ${allinfo.releaseDate}</p>
+              <p class="card-text"><b>Sensors:</b> ${allinfo.mainFeatures.sensors}</p>
+              <p class="card-text"><b>Chip Set:</b> ${allinfo.mainFeatures.chipSet}</p>
+              <p class="card-text"><b>Display Size:</b> ${allinfo.mainFeatures.displaySize}</p>
+              <p class="card-text"><b>Memory:</b> ${allinfo.mainFeatures.memory}</p>
+              <p class="card-text"><b>Storage:</b> ${allinfo.mainFeatures.storage}</p>
+              <p class="card-text"><b>Others:</b> <u>Bluetooth:</u> ${allinfo.others.Bluetooth}, <u>GPS:</u> ${allinfo.others.GPS}, <u>NFC:</u> ${allinfo.others.NFC}, <u>Radio:</u> ${allinfo.others.Radio}, <u>USB:</u> ${allinfo.others.USB}, <u>WLAN:</u> ${allinfo.others.WLAN},</p>
+            </div>
+          </div>
+    `
+    error.appendChild(div);
+}
